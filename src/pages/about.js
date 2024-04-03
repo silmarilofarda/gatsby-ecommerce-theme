edec8ef8b439749ec68b1a1100ffc1a2264b6866
Cain <silmarilofarda@gmail.com>
2024-04-03 14:34:27 -0400

    fixing an error

--- src/pages/about.js
+++ src/pages/about.js
@@ -76,31 +76,6 @@ const AboutPage = (props) => {
               </ol>
               <img alt={'founder'} src={'/about2.png'}></img>
             </div>
-//            <h3>Sustainability</h3>
-//            <div id={'#sustainability'} ref={sustainabilityRef}>
-//              <p>
-//                Our founder, Thomas Hill, had both an eye for quality and a
-//                desire to innovate. As well as using the finest fibres such as
-//                Sea Island cotton, cashmere and silk, he invented his own
-//                fabrics. Sunspel continues this commitment to innovation today
-//                and our unique fabrics include: Q100 Sea Island cotton, Q82
-//                Supima cotton, Q75 warp knit mesh cotton and Q14 warp knit
-//                cellular cotton. The technology behind these fabrics remains
-//                unchanged today and all Sunspel products use the finest cottons,
-//                wools and fibres.
-//              </p>
-//              <p>
-//                Made in Long Eaton, England and crafted from our luxurious long
-//                staple Supima cotton for unparalleled softness, comfort and
-//                durability, the Sunspel T-shirt has a classic fit and only the
-//                most essential details.{' '}
-//              </p>
-//              <p>
-//                With over 100 years spent perfecting fabric, fit and style, the
-//                Sunspel Classic T-shirt is recognised as the finest in the
-//                world.
-//              </p>
-//            </div>
           </div>
         </Container>
 
